cb01b59c95b7487c12499c3e3abebd167e8091fe
joshfranklyn <85323342+joshfranklyn@users.noreply.github.com>
2022-12-04 15:43:18 +1300

    updating

--- CS50python/cs50work.py
+++ CS50python/cs50work.py
@@ -1,1 +1,23 @@
 print ("Hello, World")
+
+# %%
+# Ask user for their name
+name = input("What is your name?")
+print ("hello, ")
+print(name)
+# Works, but not the most efficient way to write this code
+
+# %%
+name = input("What is your name?")
+print("Hello, " + name) # More efficient than before
+
+# %%
+# format string
+print(f"hello, {name}")
+
+# %%
+# String Functions
+
+# Remove whitespace from string
+name = input("What is your name?")
+name = name.strip()
