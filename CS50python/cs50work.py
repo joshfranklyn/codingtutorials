8a505e11e14d8ff80574d48e70067739183def12
joshfranklyn <85323342+joshfranklyn@users.noreply.github.com>
2022-12-05 20:06:17 +1300

    updating

--- CS50python/cs50work.py
+++ CS50python/cs50work.py
@@ -1,3 +1,4 @@
+# %%
 print ("Hello, World")
 
 # %%
@@ -8,7 +9,6 @@ print(name)
 # Works, but not the most efficient way to write this code
 
 # %%
-name = input("What is your name?")
 print("Hello, " + name) # More efficient than before
 
 # %%
@@ -18,6 +18,34 @@ print(f"hello, {name}")
 # %%
 # String Functions
 
+# All on same line, neater
+name = input("What is your name?").strip().title()
+
 # Remove whitespace from string
-name = input("What is your name?")
 name = name.strip()
+# Captialise user's name
+name = name.capitalize()
+# Title based captialisation
+name = name.title()
+
+#remove whitespace from str and captialise user's name
+name = name.strip().title()
+
+print(f"Hello, {name}")
+
+# %%
+name = input("What is your name?").strip().title()
+# Split user's name into first and last name
+first, last = name.split(" ")
+print(f"Hello, {first}")
+
+# %%
+# Calculator.py
+x = int(input("What's x? ")) # Can nest a function within another
+y = int(input("What's y? "))
+
+# z = (y) + (x)
+
+print (x + y)
+
+
